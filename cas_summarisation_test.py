ff31e82bbdc7508ab0c97e014880c0c1fe278acc
Elisha Mercado <Elisha.Mercado@Ofsted.Gov.UK>
2023-01-13 16:42:48 +0000

    Got loop working to summarise all text files

--- cas_summarisation_test.py
+++ cas_summarisation_test.py
@@ -16,6 +16,7 @@ from transformers import PegasusForConditionalGeneration as pegcondgen
 from transformers import PegasusTokenizer as pegtoken
 
 import torch
+import pandas as pd
 
 # Some complaint text
 text = """sorry for not replying sooner but it has taken a week to catch up on my sleep and disconnect from the stress. thank you for your very comprehensive email, the contents of which are noted with thanks.i cannot express enough my gratitude to you for taking the time to actually meet with me and listen fully to my concerns. after having been fighting for 6 months for someone to just hear what i was saying and offer an independent investigation was an enormous relief. that you understood my serious concerns for the safeguarding of students at hazel grove high school almost immediately improved my mental health, i had previously felt impotent, despondent and as though i was going mad. i can now feel as though i have done my very best and can leave it in the hands of the appropriate agencies and regulatory bodies to ensure any and all necessary actions are taken. i haven't received any contact so far from any of the agencies or regulatory bodies you mention and it does concern me that mark sibson was still involved in strategy meetings with the police, lado and local authority as late as last week where questions were formulated by those present to be put to me by inspector gina brennand 2 days later at a separate meeting. it would appear to me that sibson is still at the heart of the decision making within these strategy meetings and party to my whistleblowing disclosures. the fact that i have not been invited to attend any meetings either as a whistleblower or the designated safeguarding lead at hazel grove high school can only lead me to assume that he is still managing to be protected by convincing those around him that i am 'the problem'. an assumption further strengthened by his decision to notify me that i am to be made redundant with the incredulous reason given that safeguarding needs to be strengthened and made more 'robust' at hghs in light of lowe's recent arrest and remand in custody. i am very concerned that this latest move could be viewed, by those not party to my disclosures, as an indication that i am somehow responsible for lowe's conduct and the lax safeguarding at the school. another alarming incident was brought to my attention on friday when a colleague informed me that the husband of one of hghs's other assistant head teachers, mrs mel majid (who has now changed her name to miss sham) was found guilty of offences against children. mr majid was a teacher at stockport school but was suspended in september 2016 following allegations by pupils. i realise this conviction is of mrs majid's husband but for me, highlights judgement issues of her and , potentially hazel grove high school and must also raise a question of stockport's safeguarding at la level, yet again. i should also point out that it was mrs majid who was responsible for the line management of medical needs during the toogood incident in july 2016 and she has not received any sanction for her grave failings. i appreciate your offer of ongoing support and advice even though your direct involvement is concluded, your expertise and independent view is greatly valued."""
@@ -30,3 +31,28 @@ tgt_text = tokenizer.batch_decode(translated, skip_special_tokens = True)
 
 # display text summary
 tgt_text
+
+## For running all complaints ##
+
+# read in
+complaints = pd.read_csv("C:/Users/emercado/OneDrive - Ofsted/January 2023/Complaints analysis/cleaned_complaints.csv")
+
+# define summariser function
+def summariser_fn(filename, idno):
+    singlecomplaint = filename['text'][idno]
+    batch = tokenizer(singlecomplaint, truncation = True, padding = 'longest', return_tensors = "pt").to(torch_device)
+    translated = model.generate(**batch)
+    tgt_text = tokenizer.batch_decode(translated, skip_special_tokens = True)
+    
+    return tgt_text
+
+
+# get no of complaints
+x = len(complaints)
+
+output = pd.DataFrame(columns=['complaint', 'complaint_id', 'complaint_text', 'summary'])
+for i in range(0,x):
+    test = summariser_fn(complaints, i)
+    data = pd.DataFrame([[complaints['complaint'][i], complaints['complaint_id'][i], complaints['text'][i], test]], 
+                        columns = ['complaint', 'complaint_id', 'complaint_text', 'summary'])
+    output = pd.concat([output, data])
